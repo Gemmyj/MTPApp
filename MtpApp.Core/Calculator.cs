07c13618d4adc33b356f4e642dc8cd952844029b
Gemmyj <Phakawan.gem@gmail.com>
2019-01-29 17:43:29 +0700

    cul

---
+++ MtpApp.Core/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MtpApp.Core
+{
+    public class Calculator
+    {
+       public int Add(int a ,int b)
+        {
+            return a + b;
+        } 
+    }
+}
--- MtpApp/Program.cs
+++ MtpApp/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
-
+using MtpApp.Core;
 namespace MtpApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input 1 :");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Input 2 :");
+            string s2 = Console.ReadLine();
+
+            int a = Convert.ToInt32(s1);
+            int b = Convert.ToInt32(s2);
+
+            Calculator calculator = new Calculator();
+            int c = calculator.Add(a,b);
+
+            string msg = $"Your Ans : {a} + {b} = {c}";
+            Console.WriteLine(msg);
             Console.ReadLine();
         }
     }
+
+
 }
