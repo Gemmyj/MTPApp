eede3509b3f660acf955ecdba69b31caaaaede46
Gemmyj <Phakawan.gem@gmail.com>
2019-01-29 17:07:50 +0700

    Testupdate

---
+++ MtpApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MtpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
